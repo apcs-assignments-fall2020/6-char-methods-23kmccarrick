20869735ee168f763154df7005376465946b3ed7
23kmccarrick <23kmccarrick@athenian.org>
2020-09-18 16:38:21 +0000

    mayricks were compleyted

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -6,35 +6,60 @@ public class MyMain {
     // If the char is not an uppercase letter, 
     // it is returned unchanged. 
     public static char toLower(char ch) {
-        // REPLACE WITH YOUR CODE
-        return '!';
+        if(ch>90||(ch>31&&ch<65))
+            return ch;
+        else
+            return (char)(ch+32);
     }
 
     // Returns a copy of a String, where all letters 
     // are now lowercase
     public static String myToLowerCase(String str) {
-        // REPLACE WITH YOUR CODE
-        return "";
+        String meerkat = "";
+        for(int i = 0; i <str.length(); i++){
+            if(str.charAt(i)>90||(str.charAt(i)>31&&str.charAt(i)<65))
+                meerkat += str.charAt(i);
+            else
+                meerkat += (char)(str.charAt(i)+32);
+        }
+        return meerkat;
     }
 
     // Changes a lowercase letter to an uppercase letter
     // If the char is not an lowercase letter, 
     // it is returned unchanged. 
     public static char toUpper(char ch) {
-        // REPLACE WITH YOUR CODE HERE
-        return '!';
+        if(ch<97)
+            return ch;
+        else
+            return (char)(ch-32);
     }
 
     // Returns a copy of a String, where all letters 
     // are now uppercase
     public static String myToUpperCase(String str) {
-        // REPLACE WITH YOUR CODE HERE
-        return "";
+        String mayrick = "";
+        for(int i = 0; i <str.length();i++){
+            if(str.charAt(i)<97)
+                mayrick += str.charAt(i);
+            else
+                mayrick += (char)(str.charAt(i)-32);
+        }
+        return mayrick;
     }
 
 
     public static void main(String[] args) {
-        // YOUR CODE HERE
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter a string: ");
+        String result = scan.nextLine();
+        System.out.println("Would you like your string uppercase or lowercase?");
+        String muppet = scan.nextLine();
+        if(muppet.equals("lowercase"))
+            System.out.println(myToLowerCase(result));
+        else
+            System.out.println(myToUpperCase(result));
+
         
     }
 }
